4977af70abc5251379f41c86bf47059bcd502d35
Cindy Cantero <55221592+alcor15v@users.noreply.github.com>
2025-01-12 22:31:17 -0300

    Update formatting.py

--- bot/formatting.py
+++ bot/formatting.py
@@ -3,8 +3,6 @@ import discord
 
 from config import EMBED_COLOR
 
-
-
 '''Creates embed'''
 def create_embed(title, link, body):
     embed = discord.Embed(title=title[:256], url=link, description=body, color=discord.Colour.from_str(EMBED_COLOR), timestamp=datetime.now())

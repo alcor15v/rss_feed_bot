10a12cbf93727bc813262a7db8e45f76e7c6b5f2
Cindy Cantero <cindy02ra@gmail.com>
2025-01-12 22:03:14 -0300

    Put all the bot tasks in main.py

--- bot/main.py
+++ bot/main.py
@@ -1,12 +1,29 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 import bot_tasks
-from db import create_table
-from config import TOKEN
+from rss_feed import get_new_articles, extract_data
+from db import create_table, delete_old_articles, record_article_in_db
+from config import TOKEN, CHANNEL_ID, UPDATE_INTERVAL
 
 '''Creates the bot instance'''
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+'''Main task'''
+@tasks.loop(minutes=UPDATE_INTERVAL)
+async def post_new_articles_task():
+    channel = await bot.fetch_channel(CHANNEL_ID)
+    new_articles = get_new_articles()
+    for article in new_articles:
+        message = extract_data(article)
+        if message != "":
+            await channel.send(embed=message)
+        record_article_in_db(article["article"])
+
+'''Deletes old articles from the database'''
+@tasks.loop(hours=24)
+async def delete_old_articles_task():
+    delete_old_articles()
 
 '''The bot starts looping the task once it's connected'''
 @bot.listen()
